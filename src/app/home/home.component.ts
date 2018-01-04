e31c127898d29cb9e2e7d3275ebde496df897d11
Carlos Bridi <carlosbridi@gmail.com>
2018-01-04 00:12:18 -0200

    Ajustes e criações de novos arquivos de servidor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 
 @Component({
@@ -10,10 +11,16 @@ import { OfertasService } from '../ofertas.service';
 })
 export class HomeComponent implements OnInit {
 
+  public ofertas: Oferta[];
+
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit() {
-    console.log(this.ofertasService.getOfertas());
+    this.ofertasService.getOfertas2()
+    .then((abc: Oferta[]) => {
+      this.ofertas = abc; 
+      console.log('Resolveu o then depois de 3 segundos');
+    })
+    .catch((param: any) => console.log(param));
   }
-
 }
