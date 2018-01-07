e6da0396cebd8212ec4545f1ace38b572c237a1f
Carlos Bridi <carlosbridi@gmail.com>
2018-01-06 23:40:20 -0200

    Criado serviço para exibir página de restaurantes e diversão.

--- src/app/diversao/diversao.component.ts
+++ src/app/diversao/diversao.component.ts
@@ -1,15 +1,28 @@
+import { Oferta } from './../shared/oferta.model';
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [OfertasService]
+
 })
 export class DiversaoComponent implements OnInit {
 
-  constructor() { }
+
+  public ofertas: Oferta[];
+
+  constructor(private ofertaService: OfertasService) { }
 
   ngOnInit() {
+    debugger;
+    this.ofertaService.getOfertasPorCategoria('diversao')
+      .then((ofertas: Oferta[]) => {
+        this.ofertas = ofertas;
+      });
   }
 
 }
